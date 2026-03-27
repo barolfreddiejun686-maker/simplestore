858c9197f29341a67412af913480ba93db7e98bf
Fredie Jun Barol <barolfreddiejun686@gmail.com>
2026-03-27 03:34:15 -0700

    Initial commit Laravel project setup

---
+++ database/seeders/AdminSeeder.php
@@ -0,0 +1,16 @@
+<?php
+namespace Database\Seeders;
+use Illuminate\Database\Seeder;
+use App\Models\User;
+class AdminSeeder extends Seeder
+{
+public function run(): void
+{
+User::create([
+'name' => 'Admin',
+'email' => 'admin@simple-store.com',
+'password' => bcrypt('password'),
+'role' => 'admin',
+]);
+}
+}
